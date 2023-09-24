3930a50d468550924d3c7dea0545db942245967a
CHHAILENG TIM <chhailengtim2018@gmail.com>
2023-09-24 21:06:54 +0700

    string replace

---
+++ Version4/word_replace/replace_str.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = 'hi guid, welcome to hi hi hi'
+    word_to_replace = input('Enter word to replace: ')
+    word_replacement = input('Enter new word: ')
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
